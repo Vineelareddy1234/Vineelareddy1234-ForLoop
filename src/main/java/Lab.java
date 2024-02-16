7017e75d97c822d355badaed4aa7f860419bd6d2
Vineelareddy1234 <vineelareddy1232003@gmail.com>
2024-02-16 15:21:02 +0530

    string

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -41,6 +41,10 @@ public class Lab {
      */
     public String returnNumbers(int start, int end){
         String buildingString = "";
+        for(int i=start; i<end; i++){
+            buildingString+=i;
+            buildingString+=" ";
+        }
 
 //        code here
 
